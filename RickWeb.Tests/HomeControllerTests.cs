41a555aa4cb954c5c4c1c528b2d92949a13526d7
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-26 17:35:01 -0500

    First passing (web) unit tests
    Create the first passing unit test. This test exercises the
    `IndexController`. It is nice to have a passing test, but the primary
    purpose is to create the infrastructure for future unit testing.

---
+++ RickWeb.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using RickWeb.Controllers;
+
+namespace RickWeb.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [TestCase]
+        public void IndexActionReturnsIndexViewWithCorrectName()
+        {
+            var sut = new HomeController();
+
+            var actualControllerResult = (ViewResult) sut.Index();
+            Assert.That(actualControllerResult.ViewName, Is.EqualTo("Index"));
+        }
+    }
+}
