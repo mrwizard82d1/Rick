12011f737b50eba494bd012db17d75aa0d84c60a
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-28 21:50:06 -0500

    Implement class `Resistor`.
    Implement mostly "happy path" scenarios for calculating the `Resistance`
    of a `Resistor`.
    
    I believe I've captured most of the "happy path" behavior; I've noticed
    some refactoring I wish to perform. Instead of simply refactoring, I
    capture my working code (in case I make a mistake).

---
+++ Rick.Tests/ResistorTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace Rick.Tests
+{
+    /// <summary>
+    /// Defines the unit tests for the <see cref="Resistor"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Here are some additional test ideas:
+    /// * The Wikipedia article (https://en.wikipedia.org/wiki/Electronic_color_code) described a five-band resistor. 
+    ///   Because this is not needed to support the interface `IOhmValueCalculator`, I chose not to implement it.
+    /// </remarks>
+    [TestFixture]
+    public class ResistorTests
+    {
+        /// <summary>
+        /// Calculate the resistance of a resistor with a single black band.
+        /// </summary>
+        /// <remarks>
+        /// I noticed this example in the Wikipedia article (https://en.wikipedia.org/wiki/Electronic_color_code).
+        /// Because it seemed very unusual, I wanted to ensure I handled it correctly.
+        /// </remarks>
+        [TestCase]
+        public void Resistance_SingleBlackBand_ReturnsZero()
+        {
+            var sut = new Resistor(BandColor.Black, BandColor.None, BandColor.None, BandColor.None);
+
+            Assert.That(sut.Resistance(), Is.Zero);
+        }
+
+        [TestCase(BandColor.Yellow, BandColor.Violet, BandColor.Red, BandColor.Gold, 4700)]
+        [TestCase(BandColor.Red, BandColor.Red, BandColor.Orange, BandColor.Gold, 22000)]
+        [TestCase(BandColor.Yellow, BandColor.Violet, BandColor.Brown, BandColor.Gold, 470)]
+        [TestCase(BandColor.Blue, BandColor.Gray, BandColor.Black, BandColor.Gold, 68)]
+        [TestCase(BandColor.Brown, BandColor.Black, BandColor.Yellow, BandColor.Gold, 100000)]
+        [TestCase(BandColor.Orange, BandColor.Green, BandColor.Green, BandColor.Gold, 3500000)]
+        [TestCase(BandColor.Blue, BandColor.Violet, BandColor.Blue, BandColor.Gold, 67000000)]
+        [TestCase(BandColor.Gray, BandColor.White, BandColor.Violet, BandColor.Gold, 890000000)]
+        [TestCase(BandColor.Black, BandColor.Gray, BandColor.Gray, BandColor.Gold, 800000000)]
+        [TestCase(BandColor.Black, BandColor.Gray, BandColor.Gray, BandColor.Gold, 800000000)]
+        public void Resistance_SpecifiedBands_ReturnsExpectedValue(BandColor bandAColor, BandColor bandBColor,
+            BandColor bandCColor, BandColor bandDColor, int expectedValue)
+        {
+            var sut = new Resistor(bandAColor, bandBColor, bandCColor, bandDColor);
+
+            Assert.That(sut.Resistance(), Is.EqualTo(expectedValue));
+        }
+
+        [TestCase]
+        public void Resistance_TooLarge_ThrowsResistorExceptionWithMessage()
+        {
+            var sut = new Resistor(BandColor.Red, BandColor.White, BandColor.White, BandColor.None);
+
+            Assert.That(() => sut.Resistance(),
+                Throws.InstanceOf<ResistorException>().With.Message.EqualTo("Resistance '29000000000' too large."));
+        }
+    }
+}
--- Rick/Resistor.cs
+++ Rick/Resistor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Numerics;
+
 namespace Rick
 {
     /// <summary>
@@ -11,13 +14,38 @@ namespace Rick
     /// </remarks> 
     public class Resistor
     {
+        private readonly BandColor _bandAColor;
+        private readonly BandColor _bandBColor;
+        private readonly BandColor _bandCColor;
+        // ReSharper disable once NotAccessedField.Local
+        private BandColor _bandDColor;
+
         public Resistor(BandColor bandAColor, BandColor bandBColor, BandColor bandCColor, BandColor bandDColor)
         {
+            _bandAColor = bandAColor;
+            _bandBColor = bandBColor;
+            _bandCColor = bandCColor;
+            _bandDColor = bandDColor;
         }
 
         public int Resistance()
         {
-            return 20;
+            // Handle a zero-ohm resistor specially.
+            if (_bandAColor == BandColor.Black && _bandBColor == BandColor.None && _bandCColor == BandColor.None)
+            {
+                return 0;
+            }
+
+            var significantDigits = 10 * (int) _bandAColor + (int) _bandBColor;
+
+            // Using the Math library (which uses double precision arithmetic) may not be the most efficient method
+            // to calculate integral powers of 10, but is suffices for this problem.
+            var value = significantDigits * (long) Math.Pow(10, (int) _bandCColor);
+            if (value > int.MaxValue)
+            {
+                throw new ResistorException(string.Format("Resistance '{0}' too large.", value));
+            }
+            return (int) value;
         }
     }
 }
