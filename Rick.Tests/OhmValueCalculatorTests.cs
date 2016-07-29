81f7694dea8c7968dbca0944d263771376a118fb
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-28 23:00:57 -0500

    Prepared to move "too large" functionality.
    At the end of finishing the previous commit, I thought that it might be
    better to move the "too large" value check to `OhmValueCalculator` since
    the `IOhmValueCalculator` specifies return an `int`.
    
    This partitioning will allow the `Resistor` class to return 64-bit
    values for `Resistance` and letting callers decide what to do with these
    large values.
    
    This commit prepares for that movment by add a "too large" value test
    for `OhmValueCalculator`.

--- Rick.Tests/OhmValueCalculatorTests.cs
+++ Rick.Tests/OhmValueCalculatorTests.cs
@@ -6,7 +6,7 @@ namespace Rick.Tests
     [TestFixture]
     public class OhmValueCalculatorTests
     {
-//        [TestCase("Black", "", "", "", 0)]
+        [TestCase("Black", "", "", "", 0)]
         [TestCase("Red", "Black", "Black", "Brown", 20)]
         public void CalculateOhmValue_SpecifiedBands_ReturnsExpectedValue(string bandAColor, string bandBColor,
             string bandCColor, string bandDColor, int expectedResistance)
@@ -15,6 +15,15 @@ namespace Rick.Tests
 
             var actualValue = sut.CalculateOhmValue(bandAColor, bandBColor, bandCColor, bandDColor);
             Assert.That(actualValue, Is.EqualTo(expectedResistance));
+        }
+
+        [TestCase]
+        public void Resistance_TooLarge_ThrowsResistorExceptionWithMessage()
+        {
+            var sut = (IOhmValueCalculator) new OhmValueCalculator();
+
+            Assert.That(() => sut.CalculateOhmValue("red", "White", "whitE", string.Empty),
+                Throws.InstanceOf<ResistorException>().With.Message.EqualTo("Resistance '29000000000' too large."));
         }
     }
 }
